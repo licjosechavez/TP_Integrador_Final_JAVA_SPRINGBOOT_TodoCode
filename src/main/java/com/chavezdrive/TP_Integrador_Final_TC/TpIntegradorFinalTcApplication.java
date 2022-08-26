44c14a362e1d149bf90ee79886261523ccb29528
licjosechavez <t10_chavez@hotmail.com>
2022-08-26 10:19:18 -0300

    Upload proyect

---
+++ src/main/java/com/chavezdrive/TP_Integrador_Final_TC/TpIntegradorFinalTcApplication.java
@@ -0,0 +1,13 @@
+package com.chavezdrive.TP_Integrador_Final_TC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegradorFinalTcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegradorFinalTcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chavezdrive/TP_Integrador_Final_TC/TpIntegradorFinalTcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chavezdrive.TP_Integrador_Final_TC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegradorFinalTcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
